d4455d04e07bfda5a71cae206c633eb5494d7868
LePeruvienn <koto91@free.fr>
2025-10-12 22:39:27 +0200

    Added Error response handling

--- backend/src/handlers/user_handlers.rs
+++ backend/src/handlers/user_handlers.rs
@@ -4,36 +4,42 @@ use crate::models::user::User;
 use crate::forms::user_forms::FormUser;
 
 // Handler to greet a user by name from the path
-pub async fn list_all(State(pool): State<PgPool>) -> Json<Vec<User>> {
+pub async fn list_all(State(pool): State<PgPool>) -> Result<Json<Vec<User>>, StatusCode> {
 
     let query = sqlx::query_as::<_, User>("SELECT id, username, email, password, title, created_at FROM users");
 
-    let users = query.fetch_all(&pool).await.unwrap();
-
-    Json(users)
+    let users = query.fetch_all(&pool).await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?; // retourne 500 si erreur SQL
+    //
+    Ok(Json(users))
 }
 
 // Handler to greet a user by name from the path
-pub async fn get_by_id(Path(id): Path<i32>, State(pool): State<PgPool>) -> Json<User> {
+pub async fn get_by_id(Path(id): Path<i32>, State(pool): State<PgPool>) -> Result<Json<User>, StatusCode> {
 
     let query = sqlx::query_as::<_, User>("SELECT id, username, email, password, title, created_at FROM users WHERE id = $1").bind(id);
 
-    let user = query.fetch_one(&pool).await.unwrap();
+    let user = query.fetch_one(&pool).await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?; // retourne 500 si erreur SQL
 
-    Json(user)
+    Ok(Json(user))
 }
 
-pub async fn create_user(State(pool): State<PgPool>, Form(payload): Form<FormUser>) -> Json<User> {
+pub async fn create_user(State(pool): State<PgPool>, Form(payload): Form<FormUser>) -> StatusCode {
 
-    let query = sqlx::query_as::<_, User>("INSERT INTO users (username, email, password, title)")
+    let query = sqlx::query("INSERT INTO users (username, email, password, title)")
         .bind(&payload.username)
         .bind(&payload.email)
         .bind(&payload.password)
         .bind(&payload.title);
 
-    let user = query.fetch_one(&pool).await.unwrap();
+    let result = query.execute().await;
 
-    Json(user)
+    match result {
+
+        Ok(_) => StatusCode::CREATED, // 201
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR // erreur SQL
+    }
 }
 
 pub async fn delete_user(Path(id): Path<i32>, State(pool): State<PgPool>) -> StatusCode {
@@ -50,16 +56,21 @@ pub async fn delete_user(Path(id): Path<i32>, State(pool): State<PgPool>) -> Sta
     }
 }
 
-pub async fn update_user(Path(id): Path<i32>, State(pool): State<PgPool>, Form(payload): Form<FormUser>) -> Json<User> {
+pub async fn update_user(Path(id): Path<i32>, State(pool): State<PgPool>, Form(payload): Form<FormUser>) -> StatusCode {
 
-    let query = sqlx::query_as::<_, User>("UPDATE users SET username = $1, email = $2, password = $3, title = $4 WHERE id = $5)")
+    let query = sqlx::query("UPDATE users SET username = $1, email = $2, password = $3, title = $4 WHERE id = $5")
         .bind(&payload.username)
         .bind(&payload.email)
         .bind(&payload.password)
         .bind(&payload.title)
         .bind(id);
 
-    let user = query.fetch_one(&pool).await.unwrap();
+    let result = query.execute(&pool).await;
 
-    Json(user)
+    match result {
+
+        Ok(res) if res.rows_affected() > 0 => StatusCode::NO_CONTENT, // 204
+        Ok(_) => StatusCode::NOT_FOUND, // aucun utilisateur mis à jour
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR, // erreur SQL
+    }
 }
