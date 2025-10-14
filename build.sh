8cf648c75e8fce09aa752f676aa6b805a804e49e
leperuvienn <koto91@free.fr>
2025-10-14 19:25:49 +0200

    Added HTTP frotend package & improve build script

--- build.sh
+++ build.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-# ⚠️ THIS DELETE ALL VOLUMS PLEASE BE CAREFUL !
-# Remove existing volumes and containers
-docker compose down -v
+REMOVE_VOLUMES=false
 
-# Build and start your containers
+# Parse options
+while getopts "r" opt; do
+	case $opt in
+		r)
+			REMOVE_VOLUMES=true
+			;;
+		*)
+			echo "Usage: $0 [-r]"
+			exit 1
+			;;
+	esac
+done
+
+# Check if Docker service is running
+if ! systemctl is-active --quiet docker; then
+	echo "Docker is not running. Starting Docker..."
+	sudo systemctl start docker
+	# Optional: Wait a few seconds for Docker to start
+	sleep 5
+fi
+
+# Remove containers and optionally volumes
+if [ "$REMOVE_VOLUMES" = true ]; then
+	echo "⚠️ Stopping and removing containers and volumes !!!"
+	docker compose down -v
+else
+	echo "Stopping and removing containers..."
+	docker compose down
+fi
+
+# Build and start containers
 docker compose up --build
