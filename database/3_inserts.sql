31c34546034293b53e10cc1c6fe4796a268246dc
LePeruvienn <koto91@free.fr>
2025-10-22 09:11:09 +0200

    Updated Insertion Post Table & added a log file for debug

--- database/3_inserts.sql
+++ database/3_inserts.sql
@@ -1,4 +1,6 @@
--- USERS TEST DATA INSERTION
+/**************************
+* USERS TEST DATA INSERTION
+***************************/
 INSERT INTO users (username, email, password) VALUES (
 	'FEUR',
 	'feur@free.fr',
@@ -19,39 +21,33 @@ INSERT INTO users (username, email, password) VALUES (
 	'chipie@miaow.fr',
 	'jesuismims'
 );
+INSERT INTO users (username, email, password) VALUES (
+	'Sarah',
+	'saraaaah@gmail.com',
+	'parispanem'
+);
 
-INSERT INTO users (username, email, password) VALUES ('Lulu', 'lulu@chat.fr', 'password123');
-INSERT INTO users (username, email, password) VALUES ('Simba', 'simba@meow.fr', 'pawsandclaws');
-INSERT INTO users (username, email, password) VALUES ('Rocky', 'rocky@paw.fr', 'barkbark');
-INSERT INTO users (username, email, password) VALUES ('Chester', 'chester@wag.fr', 'woofwoof');
-INSERT INTO users (username, email, password) VALUES ('Sophie', 'sophie@pawtastic.fr', 'meowmeow123');
-INSERT INTO users (username, email, password) VALUES ('Coco', 'coco@feline.fr', 'snuggles');
-INSERT INTO users (username, email, password) VALUES ('Mimi', 'mimi@snug.fr', 'fuzzywuzzy');
-INSERT INTO users (username, email, password) VALUES ('Nico', 'nico@catworld.fr', 'purrfect');
-INSERT INTO users (username, email, password) VALUES ('Tina', 'tina@felinefriends.fr', 'kittycat');
-INSERT INTO users (username, email, password) VALUES ('Fifi', 'fifi@pets.fr', 'petlove');
-INSERT INTO users (username, email, password) VALUES ('Oscar', 'oscar@purr.fr', 'cuteasacat');
-INSERT INTO users (username, email, password) VALUES ('Buddy', 'buddy@pawpal.fr', 'bestfriend');
-INSERT INTO users (username, email, password) VALUES ('Bella', 'bella@purrs.fr', 'cutepaws');
-INSERT INTO users (username, email, password) VALUES ('Max', 'max@meowmeow.fr', 'fluffy123');
-INSERT INTO users (username, email, password) VALUES ('Lucy', 'lucy@kitten.com.fr', 'softpaws');
-INSERT INTO users (username, email, password) VALUES ('Pepper', 'pepper@petsclub.fr', 'happyhappy');
-INSERT INTO users (username, email, password) VALUES ('Zara', 'zara@felinezone.fr', 'playtime');
-INSERT INTO users (username, email, password) VALUES ('Lola', 'lola@thecat.fr', 'cuddles');
-INSERT INTO users (username, email, password) VALUES ('Teddy', 'teddy@pawlove.fr', 'sniffles');
-INSERT INTO users (username, email, password) VALUES ('Winnie', 'winnie@woof.fr', 'pawprints');
-INSERT INTO users (username, email, password) VALUES ('Roxy', 'roxy@animallove.fr', 'furryfriends');
-INSERT INTO users (username, email, password) VALUES ('Milo', 'milo@kitty.fr', 'sunshine');
-INSERT INTO users (username, email, password) VALUES ('Daisy', 'daisy@tailwag.fr', 'petpaw');
-INSERT INTO users (username, email, password) VALUES ('Charlie', 'charlie@pawprint.fr', 'tailspin');
-INSERT INTO users (username, email, password) VALUES ('Oliver', 'oliver@meow.com.fr', 'fluffball');
-INSERT INTO users (username, email, password) VALUES ('Cookie', 'cookie@snuggle.com.fr', 'choco123');
-INSERT INTO users (username, email, password) VALUES ('Sammy', 'sammy@pawfriends.fr', 'happycat');
-INSERT INTO users (username, email, password) VALUES ('Ruby', 'ruby@pawsome.fr', 'cutiecat');
-INSERT INTO users (username, email, password) VALUES ('Maggie', 'maggie@purrsncompany.fr', 'playful');
-INSERT INTO users (username, email, password) VALUES ('Sasha', 'sasha@pawser.com.fr', 'fuzzywuzzy123');
-INSERT INTO users (username, email, password) VALUES ('Jasper', 'jasper@felinefriends.fr', 'dreamcat');
-INSERT INTO users (username, email, password) VALUES ('Holly', 'holly@petworld.fr', 'lovefur');
-INSERT INTO users (username, email, password) VALUES ('Toby', 'toby@petpassion.fr', 'lovesnuggles');
-INSERT INTO users (username, email, password) VALUES ('Pippa', 'pippa@pawpassion.fr', 'kittensrock');
-INSERT INTO users (username, email, password) VALUES ('Benny', 'benny@furryfriends.fr', 'cuddlybear');
+/**************************
+* POSTS TEST DATA INSERTION
+***************************/
+
+INSERT INTO posts (user_id, content) VALUES (
+	1,
+	'Salut la team euuh je suis la et tout je suis feur et j''aime bien les chips en vrai et tout, vous en pensez quoi la team ?'
+);
+INSERT INTO posts (user_id, content) VALUES (
+	2,
+	'Quelqu''un connais un plan pour avoir du paté pas cher les gars, la j''ai trop faim'
+);
+INSERT INTO posts (user_id, content) VALUES (
+	3,
+	'Et hier j''était dans le bus et la ya ce gros tas de milou qui viens me parler de son paté de con putaint comment j''avais trop la haine'
+);
+INSERT INTO posts (user_id, content) VALUES (
+	4,
+	'@sarah ouvre moi la porte stp sinon je te promet que je gueule devant ta porte toute la nuit'
+);
+INSERT INTO posts (user_id, content) VALUES (
+	5,
+	'Je viens me passer à Paris Panem comment les flan vanille sont masterclass je me suis mit bien la'
+);
---
+++ database/4_logs.sql
@@ -0,0 +1,7 @@
+-- LOG USERS
+SELECT '>>> TABLE "USERS" :' AS log_message;
+SELECT * FROM users;
+
+-- LOG POSTS
+SELECT '>>> TABLE "POSTS" :' AS log_message;
+SELECT * FROM posts;
