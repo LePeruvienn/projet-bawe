a457455b6a6e878a19e6f585265805036d054de6
leperuvienn <koto91@free.fr>
2025-10-28 19:42:26 +0100

    Begin Adding Auth for the frontend

---
+++ frontend/lib/api/auth.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:http/http.dart' as http;
+
+void login (String username, String password) {
+
+}
+
+void logout() {
+
+}
